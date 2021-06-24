c6d8c37472dfcc170f5844b35b280cf3eeedc2a2
vsemi <horace.li@vsemi.io>
2021-06-24 10:49:32 -0400

    clean up

--- samples/ros/build/vsemi_tof_ros/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/vsemi/sdk/VSEMI_TOF_SDK2/samples/ros/devel/include;/home/vsemi/sdk/VSEMI_TOF_SDK2/samples/ros/src/vsemi_tof_ros/include".split(';') if "/home/vsemi/sdk/VSEMI_TOF_SDK2/samples/ros/devel/include;/home/vsemi/sdk/VSEMI_TOF_SDK2/samples/ros/src/vsemi_tof_ros/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;dynamic_reconfigure;message_runtime;sensor_msgs;std_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lvsemi_tof_cam".split(';') if "-lvsemi_tof_cam" != "" else []
-PROJECT_NAME = "vsemi_tof_ros"
-PROJECT_SPACE_DIR = "/home/vsemi/sdk/VSEMI_TOF_SDK2/samples/ros/devel"
-PROJECT_VERSION = "1.3.0"
--- samples/ros/build/vsemi_tof_ros/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;dynamic_reconfigure;message_runtime;sensor_msgs;std_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lvsemi_tof_cam".split(';') if "-lvsemi_tof_cam" != "" else []
-PROJECT_NAME = "vsemi_tof_ros"
-PROJECT_SPACE_DIR = "/home/vsemi/sdk/VSEMI_TOF_SDK2/samples/ros/install"
-PROJECT_VERSION = "1.3.0"
