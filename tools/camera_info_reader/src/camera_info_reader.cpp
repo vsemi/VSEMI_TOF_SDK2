f880707c3766c10a85558e6a6ae97ef6cf53da02
vsemi <horace.li@vsemi.io>
2021-08-25 14:14:50 -0400

    adding tools

--- tools/camera_info_reader/src/camera_info_reader.cpp
+++ tools/camera_info_reader/src/camera_info_reader.cpp
@@ -3,13 +3,13 @@
 
 #include <Camera.hpp>
 
-void test_general_info(Camera* camera)
+void read_camera_info(Camera* camera)
 {
 	ErrorNumber_e status;
 
 	uint16_t chipId, waferId;
 	status = camera->getChipInformation(chipId, waferId);
-	if (status == ERROR_NUMMBER_NO_ERROR) std::cout << "Chip:\n   wafer:   " << waferId << "\n   ID:      " << chipId << std::endl;
+	if (status == ERROR_NUMMBER_NO_ERROR) std::cout << "\nID:" << waferId << "-" << chipId << "\n   Wafer:   " << waferId << "\n   Chip:    " << chipId << std::endl;
 	else std::cerr << "Error: " << status << std::endl;
 
 	unsigned int device, version;
@@ -44,79 +44,7 @@ void test_general_info(Camera* camera)
 	} else std::cerr << "Error: " << status << std::endl;
 }
 
-void test_distance(Camera* camera, int n_frames)
-{
-	ErrorNumber_e status;
-
-	ToFImage tofImage(camera->getWidth(), camera->getHeight());
-	camera->setIntegrationTimeGrayscale(0);
-
-	clock_t start, stop;
-	start = clock();
-	for (int i = 0; i < n_frames; i ++)
-	{
-		status = camera->getDistance(tofImage);
-		if (status != ERROR_NUMMBER_NO_ERROR)
-		{
-			std::cerr << "Error: " << status << std::endl;
-			break;
-		}
-	}
-	stop = clock();
-	double interval = (double)(stop - start) / CLOCKS_PER_SEC;
-	double frame_rate = ((double) n_frames) / interval;
-	std::cout << "Distance frames: " << n_frames << " time spent: " << interval << " frame rate: " << frame_rate << std::endl;
-}
-
-void test_distance_grayscale(Camera* camera, int n_frames)
-{
-	ErrorNumber_e status;
-
-	ToFImage tofImage(camera->getWidth(), camera->getHeight());
-	camera->setIntegrationTimeGrayscale(6000);
-
-	clock_t start, stop;
-	start = clock();
-	for (int i = 0; i < n_frames; i ++)
-	{
-		status = camera->getDistanceGrayscale(tofImage);
-		if (status != ERROR_NUMMBER_NO_ERROR)
-		{
-			std::cerr << "Error: " << status << std::endl;
-			break;
-		}
-	}
-	stop = clock();
-	double interval = (double)(stop - start) / CLOCKS_PER_SEC;
-	double frame_rate = ((double) n_frames) / interval;
-	std::cout << "Distance grayscale frames: " << n_frames << " time spent: " << interval << " frame rate: " << frame_rate << std::endl;
-}
-
-void test_distance_amplitude(Camera* camera, int n_frames)
-{
-	ErrorNumber_e status;
-
-	ToFImage tofImage(camera->getWidth(), camera->getHeight());
-	camera->setIntegrationTimeGrayscale(0);
-
-	clock_t start, stop;
-	start = clock();
-	for (int i = 0; i < n_frames; i ++)
-	{
-		status = camera->getDistanceAmplitude(tofImage);
-		if (status != ERROR_NUMMBER_NO_ERROR)
-		{
-			std::cerr << "Error: " << status << std::endl;
-			break;
-		}
-	}
-	stop = clock();
-	double interval = (double)(stop - start) / CLOCKS_PER_SEC;
-	double frame_rate = ((double) n_frames) / interval;
-	std::cout << "Distance amplitude frames: " << n_frames << " time spent: " << interval << " frame rate: " << frame_rate << std::endl;
-}
-
-void test()
+void start()
 {
 	Camera* camera = Camera::usb_tof_camera_160("/dev/ttyACM0");
 	bool success = camera->open();
@@ -127,16 +55,16 @@ void test()
 		return;
 	}
 
-	test_general_info(camera);
+	read_camera_info(camera);
 
 	delete camera;
 }
 
 int main() {
 
-	std::cout << "Test starting ..." << std::endl;
+	std::cout << "Camera is starting ..." << std::endl;
 
-	test();
+	start();
 
 	return 0;
 }
